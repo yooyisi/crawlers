06e9b3dadcc2671574d0ea3077d1f154a64330a4
Qi Zhang <zhangqi@hspc14.informatik.uni-stuttgart.de>
2016-03-06 19:20:03 +0100

    class basic cw

--- crawler-house.py
+++ crawler-house.py
@@ -2,16 +2,25 @@ import urllib2
 import re
 
 page = 1
-url = 'http://stu-cn.de/forumdisplay.php?fid=14&page='+str(page)
-try:
-    request = urllib2.Request(url)
-    response = urllib2.urlopen(request)
-    #print response.read()
-    content = response.read()
-    pattern = re.compile('a href="viewthread.*?page%3D1">(.*?)</a></span>',re.S)
-    items = re.findall(pattern,content)
-    for item in items:
-        print item
-except urllib2.URLError, e:
-    if hasattr(e,"code"):
-        print e.code
+
+class HScw:
+    def __init__(self):
+        self.pageIndex = 1
+
+    def loadPage(self):
+        self.url = 'http://stu-cn.de/forumdisplay.php?fid=14&page='+str(self.pageIndex)
+        request = urllib2.Request(self.url)
+        response = urllib2.urlopen(request)
+        content = response.read()
+        pattern = re.compile('a href="viewthread.*?page%3D.">(.*?)</a></span>',re.S)
+        items = re.findall(pattern,content)
+        for item in items:
+            print item
+
+    def start(self):
+        for i in range(1,5):
+            self.pageIndex = i
+            self.loadPage()
+
+spider = HScw()
+spider.start()
